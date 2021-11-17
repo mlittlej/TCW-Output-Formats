b333079ef620dc425fa774163ccb98dafa55c376
Benjamin Fox <flamekebab@gmail.com>
2021-11-17 02:19:05 +0000

    beautified

--- tests/test_transcode_tools.py
+++ tests/test_transcode_tools.py
@@ -2,18 +2,23 @@ import tcw_transcode_tools
 import os.path
 import pytest
 
+
 def test_output_filename_sanitiser():
     # This file doesn't need to exist - it's just an example path to see whether the sanitisaton works
     test_file = "/home/flamekebab/stereo_test.wav"
     assert tcw_transcode_tools.output_filename_sanitiser(test_file) == "stereo_test"
 
+
 # A simple test encode
 def test_opus_mono_transcode():
     test_file = "tests/stereo_test.wav"
-    tcw_transcode_tools.ffmpeg_audio_transcode(test_file, "tests/", "mono_test", "opus", "24000", "1")
+    tcw_transcode_tools.ffmpeg_audio_transcode(
+        test_file, "tests/", "mono_test", "opus", "24000", "1"
+    )
 
     assert os.path.isfile("tests/mono_test.opus")
     os.remove("tests/mono_test.opus")
+
 
 # Then we can test a whole host of different options to see if any break
 transcoding_tests = [
@@ -25,11 +30,12 @@ transcoding_tests = [
 ]
 
 # Parametrize those badboys:
-@pytest.mark.parametrize('output_file, codec, q_or_b, channels', transcoding_tests)
-
+@pytest.mark.parametrize("output_file, codec, q_or_b, channels", transcoding_tests)
 def test_transcoding(output_file, codec, q_or_b, channels):
     test_file = "tests/stereo_test.wav"
-    tcw_transcode_tools.ffmpeg_audio_transcode(test_file, "tests/", output_file, codec, q_or_b, channels)
+    tcw_transcode_tools.ffmpeg_audio_transcode(
+        test_file, "tests/", output_file, codec, q_or_b, channels
+    )
 
     # It might seem silly checking whether the file exists or not but in this context ffmpeg either works or doesn't.
     # We could of course get clever and use ffmpeg to probe the files but I dread to think what that documentation looks like
